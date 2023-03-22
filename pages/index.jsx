145bd607bb6e8f6ff01cd41697cb03af674eb18a
alexandretta <alexsalazar21@gmail.com>
2023-03-22 14:38:20 -0500

    beggining styles

--- pages/index.jsx
+++ pages/index.jsx
@@ -6,12 +6,10 @@ import Footer from "@/components/footer";
 //import '../styles/app.css'
 
 export default function Home() {
-
   const [products, setProducts] = useState([]);
   useEffect(() => {
     allProducts(setProducts);
   }, []);
-
 
   return (
     <>
@@ -21,17 +19,30 @@ export default function Home() {
         <h1>Productos</h1>
         {products.map((prod) => (
           <article key={prod.id} className='productCard'>
-            <img alt=''  src={prod.images[0]}></img>
+            <img alt='' src={prod.images[0]}></img>
             <h1>{prod.title}</h1>
             <p>{prod.description}</p>
             <p>{prod.price}</p>
-            
           </article>
         ))}
       </section>
       <section></section>
 
       <Footer />
+      <style jsx>{`
+        .productCard {
+          display: flex;
+          flex: grid;
+          width: 120px;
+          height: 240px;
+          text-align: center;
+          background-color: white;
+        }
+
+        .productCard h1 {
+          font-weight: bold;
+        }
+      `}</style>
     </>
   );
 }
